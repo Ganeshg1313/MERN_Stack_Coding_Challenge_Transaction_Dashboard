152f3f372679b23f1cd1ca1cdee322debd8ca748
Ganeshg1313 <ganeshcoder333@gmail.com>
2024-06-23 14:55:34 +0530

    version 1.5  backend resolved error

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,6 @@ const barChartRoute = require('../routes/barChart');
 const pieChartRoute = require('../routes/pieChart');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
